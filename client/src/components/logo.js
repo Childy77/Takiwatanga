4803b8e6feb613cc6cd9ac7031842281975d2ed5
Ryan <ryanbairdchilders@gmail.com>
2023-04-20 12:09:38 -0700

    stared landing page

---
+++ client/src/components/logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroImage from "../assets/Autism-Hands.png";
+
+const Logo = () => {
+return (
+    <div id="logo">
+<img src={HeroImage} alt="Photo of autism hands"/>
+    </div>
+)
+}
+
+export default Logo
