1f5d38348ff92799ae0e1934b40e222ac81b0141
Ryan <ryanbairdchilders@gmail.com>
2023-04-24 20:43:31 -0700

    added profile page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import Logo from '../components/logo';
+import About from '../components/about';
+import Start from '../components/sign-up';
+
+function Home() {
+  return (
+    <>
+    <Logo/>
+    <About/>
+    <Start/>
+    </>
+  )
+}
+
+export default Home
