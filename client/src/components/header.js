303bd75f06bed13e28cd5a61758e39fbec29c52c
Ryan <ryanbairdchilders@gmail.com>
2023-04-26 18:41:13 -0700

    added new pages

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -9,14 +9,17 @@ const Header = () => {
     return (
       <Navbar className="navbar" bg="light" expand="lg">
       <Container>
-        <Navbar.Brand id="Name" href="#home">Takiwatanga</Navbar.Brand>
+        <Navbar.Brand id="Name" href="#home"><i class="fa-solid fa-hands-holding-child"></i> Takiwatanga</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Link className="nav-link" to="/">Home</Link>
-            <Link className="nav-link" href="#link">About Us</Link>
+            <Link className="nav-link" to="/ourstory">Our Story</Link>
             <NavDropdown title="User" id="basic-nav-dropdown">
               <Link className="dropdown-item" to="/profile">Profile</Link>
+              <NavDropdown.Item href="#action/3.2">
+                Log in
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Logout
               </NavDropdown.Item>
---
+++ client/src/pages/OurStory.js
@@ -0,0 +1,69 @@
+
+
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Digory from "../assets/Dig1.jpg";
+// import Digory from "../assets/Dig1.jpg";
+// import Digory from "../assets/Dig1.jpg";
+// import Digory from "../assets/Dig1.jpg";
+
+
+import CardGroup from 'react-bootstrap/CardGroup';
+        
+        function OurStory() {
+          return (
+            <CardGroup>
+      <Card>
+        <Card.Img variant="top" src={Digory} alt="Photo of a child" />
+        <Card.Body>
+          <Card.Title>Ryan Childers</Card.Title>
+          <Card.Text>
+          I have a child with Autism.  His name is Digory.  We were suspicious of his condition at a very early age.  We then had him diagnosed, and proceeded to find him the best possible therapies and educational environment possible for him.  It has been difficult, but our love for him and hope for his future grows every day.  It is my goal with this site to be able share and support one another through our struggles and victories and people with special needs.
+          </Card.Text>
+        </Card.Body>
+        
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={Digory} alt="Photo of a child" />
+        <Card.Body>
+          <Card.Title>Colton Taylor-Ortiz</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={Digory} alt="Photo of a child" />
+        <Card.Body>
+          <Card.Title>Ashley Arena</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={Digory} alt="Photo of a child" />
+        <Card.Body>
+          <Card.Title>Johann Kubeja</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        
+      </Card>
+    </CardGroup>
+    
+            
+          );
+        }
+        
+        
+        export default OurStory;
+    
