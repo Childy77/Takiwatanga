ed0331dc400490cecc8adfe3c0c122b330815da7
Ryan <ryanbairdchilders@gmail.com>
2023-04-28 15:16:53 -0700

    fixed post.  We can add a post

--- client/src/components/createPost.js
+++ client/src/components/createPost.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
+import { ADD_POST } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const CreatePost = () => {
   const [formState, setFormState] = useState({
-    description: '',
-    url: ''
+    title: '',
+    postText: '',
+    postAuthor: ''
+  });
+
+  const [createPost] = useMutation(ADD_POST, {
+    variables: {
+    //   description: formState.title,
+      postText: formState.postText,
+      postAuthor: "bob"
+    }
   });
 
   return (
@@ -11,30 +22,31 @@ const CreatePost = () => {
       <form className="post"
         onSubmit={(e) => {
           e.preventDefault();
+          createPost();
         }}
       >
-        <label for="title">Title</label>
+        <label htmlFor="title">Title</label>
           <input id='title'
             className="mb2"
-            value={formState.description}
+            value={formState.title}
             onChange={(e) =>
               setFormState({
                 ...formState,
-                description: e.target.value
+                title: e.target.value
               })
             }
             type="text"
             placeholder="Your post title"
           />
-          <label for="post-content">Post here</label>
-          <textarea rows="5"
+          <label htmlFor="post-text">Post here</label>
+          <textarea id='post-text' rows="5"
             
             className="mb2"
-            value={formState.url}
+            value={formState.postText}
             onChange={(e) =>
               setFormState({
                 ...formState,
-                url: e.target.value
+                postText: e.target.value
               })
             }
             
