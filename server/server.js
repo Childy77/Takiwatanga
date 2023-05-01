25060b6186ce5a4de2697daad48d18d54e368d5a
Ryan <ryanbairdchilders@gmail.com>
2023-05-01 11:50:38 -0700

    final apollo fixes

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 
-const startApolloServer = async () => {
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
   
@@ -35,4 +35,4 @@ const startApolloServer = async () => {
   })
   };
 
-  startApolloServer();
+  startApolloServer(typeDefs, resolvers);
